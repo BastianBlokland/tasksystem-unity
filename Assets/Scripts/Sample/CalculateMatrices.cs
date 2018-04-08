40eedd9453ebee4dc311a7076a3c992c85353545
Bastian Blokland <bastian@armadainteractive.com>
2018-04-08 19:04:26 +0300

    Created a task for calculating the matrices

---
+++ Assets/Scripts/Sample/CalculateMatrices.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Tasks;
+using UnityEngine;
+using Utils;
+
+namespace Sample
+{
+	public struct CalculateMatrices : ITask<CubeData, Matrix4x4>
+	{
+		public void Execute(ref CubeData data, ref Matrix4x4 matrix)
+		{
+			float rotInRad = data.Rotation * Mathf.Deg2Rad;
+			float cos = Mathf.Cos(rotInRad);
+			float sin = Mathf.Sin(rotInRad);
+			matrix = new Matrix4x4
+			(
+				column0: new Vector4(cos, 0f, -sin, 0f),
+				column1: new Vector4(0f, 1f, 0f, 0f),
+				column2: new Vector4(sin, 0f, cos, 0f),
+				column3: new Vector4(data.Position.x, 0f, data.Position.y, 1f)
+			);
+		}
+	}
+}
--- Assets/Scripts/Sample/SampleController.cs
+++ Assets/Scripts/Sample/SampleController.cs
@@ -15,6 +15,7 @@ namespace Sample
 		[SerializeField] private int batchSize = 10;
 
 		private readonly CubeData[] cubeData = new CubeData[CUBE_COUNT];
+		private readonly Matrix4x4[] cubeMatrices = new Matrix4x4[CUBE_COUNT];
 		private readonly PartitionSet<CubeData> partitionedData = new PartitionSet<CubeData>();
 	
 		private ITaskHandle updateCubeTask;
@@ -44,12 +45,18 @@ namespace Sample
 			if(updateCubeTask != null)
 				updateCubeTask.Join();
 
-			//Handle the new cubedata
+			//Calculate the new matrices
+			ITaskHandle calculateMatricesTask = taskManager.ScheduleArray(cubeData, cubeMatrices, new CalculateMatrices());
+			calculateMatricesTask.Join();
+
+			//Prepare the data for rendering
 			renderSet.Clear();
 			for (int i = 0; i < CUBE_COUNT; i++)
-			{
-				renderSet.Add(CreateMatrix(cubeData[i]));
+				renderSet.Add(cubeMatrices[i]);
 
+			//Respawn the cubes if too far away
+			for (int i = 0; i < CUBE_COUNT; i++)
+			{
 				if(cubeData[i].Position.sqrMagnitude > (200f * 200f))
 					SpawnCube(i);
 			}
@@ -91,20 +98,6 @@ namespace Sample
 				Velocity = Vector2.zero,
 				Rotation = 0f
 			};
-		}
-
-		private Matrix4x4 CreateMatrix(CubeData data)
-		{
-			float rotInRad = data.Rotation * Mathf.Deg2Rad;
-			float cos = Mathf.Cos(rotInRad);
-			float sin = Mathf.Sin(rotInRad);
-			return new Matrix4x4
-			(
-				column0: new Vector4(cos, 0f, -sin, 0f),
-				column1: new Vector4(0f, 1f, 0f, 0f),
-				column2: new Vector4(sin, 0f, cos, 0f),
-				column3: new Vector4(data.Position.x, 0f, data.Position.y, 1f)
-			);
 		}
 	}
 }
