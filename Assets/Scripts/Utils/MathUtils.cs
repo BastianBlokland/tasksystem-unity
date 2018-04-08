83d6174ad1f72d14ad7dfc0e2fbf4bed284526c7
Bastian Blokland <bastian@armadainteractive.com>
2018-04-08 21:41:50 +0300

    Created simple 'FastNormalize' using quack's https://en.wikipedia.org/wiki/Fast_inverse_square_root

---
+++ Assets/Scripts/Utils/MathUtils.cs
@@ -0,0 +1,38 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Utils
+{
+	public static class MathUtils
+	{
+		[StructLayout(LayoutKind.Explicit, Size=4)]
+		private struct IntFloat 
+		{
+			[FieldOffset(0)]
+			public float FloatValue;
+
+			[FieldOffset(0)]
+			public int IntValue;
+		}
+
+		//Implementation based on: https://en.wikipedia.org/wiki/Fast_inverse_square_root
+		public static float FastInvSqrRoot(float number)
+		{
+			const float threehalfs = 1.5f;
+
+			IntFloat conv = new IntFloat { FloatValue = number }; 
+
+			float x2 = number * .5f;
+	 		conv.IntValue = 0x5f3759df - (conv.IntValue >> 1);
+	 		conv.FloatValue = conv.FloatValue * (threehalfs - (x2 * conv.FloatValue * conv.FloatValue));
+	 		return conv.FloatValue;
+		}
+
+		public static Vector2 FastNormalize(Vector2 vector)
+		{
+			float sqrMag = vector.sqrMagnitude;
+			float invSqrRoot = FastInvSqrRoot(sqrMag);
+			return new Vector2(vector.x * invSqrRoot, vector.y * invSqrRoot);
+		}
+	}
+}
