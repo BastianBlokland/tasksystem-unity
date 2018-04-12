9d0f7ae49214fb5aa1990a95b2d886ad252e1563
Bastian Blokland <bastianblokland@gmail.com>
2018-04-13 02:33:49 +0300

    Tweaked the sample scene

--- Assets/Scripts/Sample/Tasks/AddToRenderSetTask.cs
+++ Assets/Scripts/Sample/Tasks/AddToRenderSetTask.cs
@@ -19,18 +19,19 @@ namespace Sample
 
 		public void Execute(ref CubeData cube, int index, int batch)
 		{
-			float scale = Mathf.Lerp(3f, 1f, Mathf.Min(cube.TimeNotHitTarget1, cube.TimeNotHitTarget2) * .5f);
 			Matrix4x4 matrix = new Matrix4x4
 			(
-				column0: new Vector4(scale, 0f, 0f, 0f),
-				column1: new Vector4(0f, scale, 0f, 0f),
-				column2: new Vector4(0f, 0f, scale, 0f),
+				column0: new Vector4(1f, 0f, 0f, 0f),
+				column1: new Vector4(0f, 1f, 0f, 0f),
+				column2: new Vector4(0f, 0f, 1f, 0f),
 				column3: new Vector4(cube.Position.x, 0f, cube.Position.y, 1f)
 			);
 
 			Vector4 color = Color.gray;
-			color = Vector4.Lerp(HitByTarget1Color, color, cube.TimeNotHitTarget1 * .1f);
-			color = Vector4.Lerp(HitByTarget2Color, color, cube.TimeNotHitTarget2 * .1f);
+			if(cube.TimeNotHitTarget1 < 10f)
+				color = Vector4.Lerp(HitByTarget1Color, color, cube.TimeNotHitTarget1 * .1f);
+			if(cube.TimeNotHitTarget2 < 10f)
+				color = Vector4.Lerp(HitByTarget2Color, color, cube.TimeNotHitTarget2 * .1f);
 
 			renderSet.Add(batch, matrix, color);
 		}
